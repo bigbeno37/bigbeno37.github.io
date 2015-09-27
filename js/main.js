b40d03c9b6a77c1072cff8626c73a2c0d5a24cc2
bigbeno37 <bigbeno37@gmail.com>
2015-09-27 17:18:46 +1000

    Jekyll blog

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	onResize();
-
-	$(window).resize(function(){ onResize() });
-});
-
-function onResize(){
-	$(".title h1").css("font-size", $(window).width()*0.12);
-}
