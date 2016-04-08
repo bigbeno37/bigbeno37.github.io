154e305509c14b5f44d785c54bc70103e3aef7e3
bigbeno37 <bigbeno37@gmail.com>
2016-04-08 17:31:56 +1000

    Made site mobile-friendly

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
-//
+function switchIcon(active) {
+    if (active) {
+        $(".hamburgerIcon i").removeClass("fa-times");
+        $(".hamburgerIcon i").addClass("fa-bars");
+
+        return false;
+    }
+
+    else {
+        $(".hamburgerIcon i").removeClass("fa-bars");
+        $(".hamburgerIcon i").addClass("fa-times");
+
+        return true;
+    }
+}
+
+$(document).ready(function(){
+    var active = false;
+
+    $(".dropdownMenu").hide();
+    
+    $(".hamburgerIcon").click(function(){
+        active = switchIcon(active);
+
+        $(".dropdownMenu").slideToggle("slow", function(){});
+    });
+});
