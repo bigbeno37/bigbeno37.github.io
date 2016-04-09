efec588e8e6347b61dbe43740922e1181027984e
bigbeno37 <bigbeno37@gmail.com>
2016-04-09 21:20:20 +1000

    Finally realised how silly the popups were, and added more responsiveness and created new sections on the page

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,12 @@
 function switchIcon(active) {
     if (active) {
-        $(".hamburgerIcon i").removeClass("fa-times");
-        $(".hamburgerIcon i").addClass("fa-bars");
+        $(".hamburgerIcon i").removeClass("fa-times").addClass("fa-bars");
 
         return false;
     }
 
     else {
-        $(".hamburgerIcon i").removeClass("fa-bars");
-        $(".hamburgerIcon i").addClass("fa-times");
+        $(".hamburgerIcon i").removeClass("fa-bars").addClass("fa-times");
 
         return true;
     }
@@ -18,88 +16,12 @@ function switchIcon(active) {
 
 $(document).ready(function(){
     var active = false;
-    var aboutMePopupIsVisible = false;
-    var projectsPopupIsVisible = false;
 
     $(".dropdownMenu").hide();
-    $(".projects").hide();
-    $(".aboutMe").hide();
     
     $(".hamburgerIcon").click(function(){
         active = switchIcon(active);
 
         $(".dropdownMenu").slideToggle("slow", function(){});
-    });
-    
-    $(".aboutMeButton").click(function(){
-        if (aboutMePopupIsVisible){
-            $(".aboutMe").addClass("fadeOutDownBig");
-
-            aboutMePopupIsVisible = false;
-
-            setTimeout(function () {
-                $(".aboutMe").hide();
-            }, 800);
-        }
-
-        else{
-            $(".aboutMe").show().addClass("fadeInUpBig").removeClass("fadeOutDownBig");
-
-            aboutMePopupIsVisible = true;
-        }
-    });
-
-    $(".closeButton").click(function(){
-        if (aboutMePopupIsVisible){
-            $(".aboutMe").addClass("fadeOutDownBig");
-
-            aboutMePopupIsVisible = false;
-
-            setTimeout(function () {
-                $(".aboutMe").hide();
-            }, 800);
-        }
-
-        else{
-            $(".aboutMe").show().addClass("fadeInUpBig").removeClass("fadeOutDownBig");
-
-            aboutMePopupIsVisible = true;
-        }
-    });
-
-    $(".projectsButton").click(function(){
-        if (projectsPopupIsVisible){
-            $(".projects").addClass("zoomOut");
-
-            projectsPopupIsVisible = false;
-
-            setTimeout(function () {
-                $(".projects").hide();
-            }, 800);
-        }
-
-        else{
-            $(".projects").show().addClass("zoomIn").removeClass("zoomOut");
-
-            projectsPopupIsVisible = true;
-        }
-    });
-
-    $(".projectsCloseButton").click(function(){
-        if (projectsPopupIsVisible){
-            $(".projects").addClass("zoomOut");
-
-            projectsPopupIsVisible = false;
-
-            setTimeout(function () {
-                $(".projects").hide();
-            }, 800);
-        }
-
-        else{
-            $(".projects").show().addClass("zoomIn").removeClass("zoomOut");
-
-            projectsPopupIsVisible = true;
-        }
     });
 });
