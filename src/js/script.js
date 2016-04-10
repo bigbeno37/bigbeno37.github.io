6f583249890beae88a18590ae9b9f66d858b1b94
bigbeno37 <bigbeno37@gmail.com>
2016-04-10 16:52:41 +1000

    Added smooth scrolling when clicking on buttons

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,23 @@
+// Created by Devin Sturgeon found in comments section of
+// https://css-tricks.com/snippets/jquery/smooth-scrolling/
+
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            $('html,body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
+    }
+});
+
+/* -------------------------------------------------- */
+
 function switchIcon(active) {
     if (active) {
         $(".hamburgerIcon i").removeClass("fa-times").addClass("fa-bars");
